6bc463599416bf0e01d17228639f524f5cbea87a
odonnellnoel <odonnellnoel@github>
2019-01-06 01:03:29 +0000

    Stopped using std::pairs to hold boolean telling if streams were files or not, instead using dynamic casts to discover type

--- src/main.cpp
+++ src/main.cpp
@@ -43,11 +43,10 @@ MCKEY check_get_mckey(const variables_map& map) {
 /**
  * Returns an input stream which may be a file stream if the filename is present in the optional,
  * or else std::cin. If there's a problem opening the file, an error will be thrown.
- * A std::pair is used to also return a boolean which indicates if it's an ifstream or not.
  */
-pair<istream&, bool> get_input_stream(const optional<string>& in_filename) {
+istream& get_input_stream(const optional<string>& in_filename) {
     if (in_filename == nullopt) {
-        return pair<istream&, bool>(cin, false);
+        return cin;
     }
     
     ifstream* infile = new ifstream(in_filename.value(), ios::in|ios::binary);
@@ -55,18 +54,17 @@ pair<istream&, bool> get_input_stream(const optional<string>& in_filename) {
         throw error("Input file " + in_filename.value() + " is not good");
     }
     
-    return pair<istream&, bool>(*infile, true);
+    return *infile;
 }
 
 
 /**
  * Returns an output stream which may be a file stream if the filename is present in the optional,
  * or else std::cout. If there's a problem opening the file, an error will be thrown.
- * A std::pair is used to also return a boolean which indicates if it's an ofstream or not.
  */
-pair<ostream&, bool> get_output_stream(const optional<string>& out_filename) {
+ostream& get_output_stream(const optional<string>& out_filename) {
     if (out_filename == nullopt) {
-        return pair<ostream&, bool>(cout, false);
+        return cout;
     }
     
     ofstream* outfile = new ofstream(out_filename.value(), ios::out|ios::binary);
@@ -74,39 +72,29 @@ pair<ostream&, bool> get_output_stream(const optional<string>& out_filename) {
         throw error("Output file " + out_filename.value() + " is not good");
     }
     
-    return pair<ostream&, bool>(*outfile, true);
+    return *outfile;
 }
 
 
 /**
- * Closes any input stream as returned from get_input_stream. There are 2 cases:
- * cin and file. Nothing needs to be done in the case of cin. If it's a file,
- * the file stream shall be closed and the associated ifstream destructed.
+ * Close and delete `stream` if it's an ifstream, otherwise do nothing
  */
-void close_input_stream(const pair<istream&, bool>& input_pair) {
-    istream& input_stream = input_pair.first;
-    bool is_file = input_pair.second;
-    
-    if (is_file) {
-        ((ifstream*)&input_stream)->close();
-        delete &input_stream;
-    }
+void try_close_ifstream(istream& stream) {
+    try {
+        dynamic_cast<ifstream&>(stream).close();
+        delete& stream;
+    } catch(std::bad_cast&) {}
 }
 
 
 /**
- * Closes any output stream as returned from get_output_stream. There are 2 cases:
- * cout and file. Nothing needs to be done in the case of cout. If it's a file,
- * the file stream shall be closed and the associated ofstream destructed.
+ * Close and delete `stream` if it's an ofstream, otherwise do nothing
  */
-void close_output_stream(const pair<ostream&, bool>& output_pair) {
-    ostream& output_stream = output_pair.first;
-    bool is_file = output_pair.second;
-    
-    if (is_file) {
-        ((ofstream*)&output_stream)->close();
-        delete &output_stream;
-    }
+void try_close_ofstream(ostream& stream) {
+    try {
+        dynamic_cast<ofstream&>(stream).close();
+        delete& stream;
+    } catch(std::bad_cast&) {}
 }
 
 
@@ -148,14 +136,15 @@ int main(int argc, char **argv) {
             throw error("Input file cannot be same as output file");
         }
         
-        pair<istream&, bool> input_pair = get_input_stream(in_filename);
-        pair<ostream&, bool> output_pair = get_output_stream(out_filename);
-        
-        microcipher_process(mckey, input_pair.first, output_pair.first);
-        
-        close_input_stream(input_pair);
-        close_output_stream(output_pair);
-        
+        istream& is = get_input_stream(in_filename);
+        ostream& os = get_output_stream(out_filename);
+
+        // Perform the encryption or decryption
+        microcipher_process(mckey, is, os);
+
+        try_close_ifstream(is);
+        try_close_ofstream(os);
+
         return 0;
     } catch(const error& ex) {
         cerr << ex.what() << endl;
